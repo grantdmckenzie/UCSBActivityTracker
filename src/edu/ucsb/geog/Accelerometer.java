538fbc51510b1a27bf9e40cee355533575b17c63
Yingjie Hu <yjhu.geo@gmail.com>
2012-06-02 17:05:40 -0700

    first commit

--- src/edu/ucsb/geog/Accelerometer.java
+++ src/edu/ucsb/geog/Accelerometer.java
@@ -1,29 +1,36 @@
 package edu.ucsb.geog;
 
-import android.content.Context;
+import java.util.HashMap;
+import java.util.Observable;
+
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.widget.TextView;
 
-public class Accelerometer extends TextView implements SensorEventListener
+public class Accelerometer extends Observable implements SensorEventListener, Runnable
 {
 
 	private SensorManager mSensorManager;
-	private Sensor mAccelerometer;
+	private Sensor mAccelerometer;	
+	private long msInterval; 
+	private HashMap<String, Double> fix;
+	private double accelx = 0;
+	private double accely = 0;
+	private double accelz = 0;
 	
-	public Accelerometer(Context context, SensorManager mSensorManager) {
-		super(context);
-				
+	public Accelerometer(SensorManager mSensorManager, long msInterval) {
+					
 		this.mSensorManager = mSensorManager;
-		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);			
+		mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		this.msInterval = msInterval;
+		fix =  new HashMap<String, Double>();
 		
 	}
 	
 	public void startRecording()
 	{
-		mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI);
+		mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 	}
 	
 	public void stopRecording()
@@ -38,11 +45,41 @@ public class Accelerometer extends TextView implements SensorEventListener
 	}
 
 	@Override
-	public void onSensorChanged(SensorEvent event) {
+	public void onSensorChanged(SensorEvent event)
+	{
 		if (event.sensor.getType() != Sensor.TYPE_ACCELEROMETER) return;
+				
+		accelx = event.values[0];
+		accely = event.values[1];
+		accelz = event.values[2];
+						
+	}
+	
+
+	
+	@Override
+	public void run() 
+	{
+		fix =  new HashMap<String, Double>();
+		fix.put("accelx", accelx);
+		fix.put("accely", accely);
+		fix.put("accelz", accelz);
 		
-		// TODO Auto-generated method stub
-	    this.setText(event.values[0]+","+event.values[1]+","+event.values[2]);
+		setChanged();
+		notifyObservers();
+		
+		try 
+		{
+			Thread.sleep(msInterval);             
+		} 
+		catch (InterruptedException ex) 
+		{
+		}	
+	}
+	
+	public HashMap getFix() 
+	{
+		return fix;
 	}
 
 }
--- src/edu/ucsb/geog/UCSBActivityTrackerActivity.java
+++ src/edu/ucsb/geog/UCSBActivityTrackerActivity.java
@@ -1,40 +1,100 @@
 package edu.ucsb.geog;
 
-import android.app.Activity;
-import android.hardware.SensorManager;
-import android.os.Bundle;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
 
-public class UCSBActivityTrackerActivity extends Activity {
-	
-	
-	private SensorManager mSensorManager; 
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.SensorManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class UCSBActivityTrackerActivity extends Activity implements Observer {
+
+	private TextView mAccelerometerDisplay;
+	private TextView mWifiDisplay;
+
+	private SensorManager mSensorManager;
 	private Accelerometer accelerometer;
+
+	private WifiManager mWifiManager;
+	private BroadcastReceiver wifiReceiver = new BroadcastReceiver()
+	{
+		@Override
+		public void onReceive(Context ctx, Intent intent)
+		{
+			List <ScanResult> scanresults = mWifiManager.getScanResults();
+			mWifiDisplay.setText("\nThe number of available wifi networks is: "+scanresults.size());
+			mWifiDisplay.append("\n'Wifi Name'\t'Wifi signal'");
+			for (ScanResult sr: scanresults)
+			{
+				mWifiDisplay.append("\n"+sr.SSID);
+				mWifiDisplay.append("\t"+sr.level);
+			}
+
+		}
+	};
 	
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        
-        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        accelerometer = new Accelerometer(this, mSensorManager);
-        
-        setContentView(accelerometer);
-    }
-    
-    @Override
-    protected void onResume() {
-    	// TODO Auto-generated method stub
-    	super.onResume();
-    	accelerometer.startRecording();
-    }
-    
-    @Override
-    protected void onPause() {
-    	// TODO Auto-generated method stub
-    	super.onPause();
-    	accelerometer.stopRecording();
-    	
-    }
+	private HashMap fix;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		// initiate display textviews
+		mAccelerometerDisplay = (TextView)findViewById(R.id.accelerometerDisplay);
+		mWifiDisplay = (TextView)findViewById(R.id.wifiDisplay);
+
+		// initiate variables for accelerometer
+		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		accelerometer = new Accelerometer(mSensorManager, 500);
+		accelerometer.addObserver(this);
+
+		// initiate variables for wifi
+		mWifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+		IntentFilter filter = new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);       
+		registerReceiver(wifiReceiver, filter);
+		mWifiManager.startScan();
+
+
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		accelerometer.startRecording();
+		//
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		accelerometer.stopRecording();   
+	}
+
+	@Override
+	public void update(Observable observable, Object data) 
+	{
+		if(observable instanceof Accelerometer)
+		{
+			fix  = ((Accelerometer)observable).getFix();
+			mAccelerometerDisplay.setText("Accelerometer: "+fix.get("accelx")+" "+fix.get("accely")+" "+fix.get("accelz"));
+			
+		}
+		
+		
+		
+	}
 }
