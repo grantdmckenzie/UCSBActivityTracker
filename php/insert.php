7778aec802992f54f40c2239c70c2bf502a12a82
Grant McKenzie <grantdmckenzie@gmail.com>
2012-06-12 12:15:52 -0700

    PHP code for splitting out the sensor types in to different tables.

--- php/insert.php
+++ php/insert.php
@@ -10,25 +10,36 @@
 
 	$d = json_decode(stripslashes($data));
 	sortData($d, $deviceid);
-	print_r($d);
 	
-	 // basicdata VALUES ('','".$deviceid."','".$d[0]->sensor."')";
-
+	// print_r($d) ."<br/><br/><br/>";
+	// basicdata VALUES ('','".$deviceid."','".$d[0]->sensor."')";
 	// mysql_query($query) or die(mysql_error());
 	// echo mysql_insert_id();
-
 
 	function sortData($d, $deviceid) {
 		$i=0;
 		while($i<count($d)) {
-			$query = "INSERT INTO ";
 			switch($d[$i]->sensor) {
 				case 1:
-					$query .= "accelerometer VALUES ('','".$deviceid."', ".$d[$i]->accelx .",". $d[$i]->accely .",". $d[$i]->accelz .",". $d[$i]->ts.")";
+					$query = "INSERT INTO accelerometer VALUES ('','".$deviceid."', ".$d[$i]->accelx .",". $d[$i]->accely .",". $d[$i]->accelz .",". $d[$i]->ts.");";
+					mysql_query($query) or die(mysql_error());
 					break;
 				case 2:
+					foreach($d[$i] as $key => $value) {
+						if ($key != "sensor" && $key != "ts") {
+							$details = explode(",", $value);
+							$assid = explode(":", $details[0]);
+							$capabilities = explode(":", $details[2]);
+							$level = explode(":", $details[3]);
+							$frequency = explode(":", $details[4]);
+							$query = "INSERT INTO wifi VALUES ('','".$deviceid."',".$d[$i]->ts.",'".$assid[1]."','".$details[1]."','".$capabilities[1]."',".$level[1].",".$frequency[1].");";
+							mysql_query($query) or die(mysql_error());
+						}
+			    	}
 					break;
 				case 3:
+					$query = "INSERT INTO coordinates VALUES ('','".$deviceid."', ".$d[$i]->lat .",". $d[$i]->lng .",". $d[$i]->speed .",". $d[$i]->accuracy.",". $d[$i]->altitude.",". $d[$i]->ts.");";
+					mysql_query($query) or die(mysql_error());
 					break;
 				default:
 			}
