739efc548ec7569e873b694d857d79823b5b28e9
liwwchina <liww.china@gmail.com>
2012-06-13 15:03:17 -0700

    Update master

--- src/edu/ucsb/geog/Wifi.java
+++ src/edu/ucsb/geog/Wifi.java
@@ -39,25 +39,53 @@ public class Wifi extends Observable implements Runnable {
 	}
     
     
-    public void run(){
+	public void run(){
 
     	while(running)
    		 {
 		       	 
     			 try {
     				 List <ScanResult> scanresults = wifi.getScanResults();
-    				 timestamp = new Long(System.currentTimeMillis()/1000);
-    		         fix.put("sensor", 2.0);
-    		         fix.put("ts", timestamp);
-    		         count = 0;
-    		         for (ScanResult sr: scanresults)
-    		         {
-    		         	fix.put(count+"", sr);
-    		         	count++;
-    		         }
-		         setChanged();
-		         notifyObservers(fix);
-		         Thread.sleep(interval);
+    				 timestamp = new Long(System.currentTimeMillis()/1000);			 
+    				 //determine whether the wifi signal has changed
+    				 boolean flag = false; //default state: not to reset fix
+    				 if(fix!=null&&(fix.length()-2==scanresults.size()))//wifi signal changed
+    				{
+    					 for(ScanResult sr:scanresults){
+    						 if(fix.has(sr.BSSID))
+    						{
+    							 JSONObject wifirecord = (JSONObject)fix.get(sr.BSSID);
+    							 if(!wifirecord.get("SSID").equals(sr.SSID)||
+    									 (Integer)wifirecord.get("Signal")!=sr.level||
+    									 (Integer)wifirecord.get("Frequency")!=sr.frequency||
+    									 !wifirecord.get("Capabilities").equals(sr.capabilities)){
+    								 flag = true;
+    								 break;
+    							 }
+    						}
+    					 }
+    				}else if(fix!=null&&(fix.length()-2!=scanresults.size())) //number of available wifi changed
+    					flag = true;
+    				//only when # of wifi changed or wifi signal changes, reset fix and notify observer, otherwise, sleep and wait to check next return
+    				 //Log.v("flag", flag+" (true mean need update, false means no need to update)");
+    				 if(flag){
+    					 fix = new JSONObject();
+	    				 fix.put("sensor", 2.0);
+	    		         fix.put("ts", timestamp);
+	    		         for (ScanResult sr: scanresults)
+	    		         {
+	    		        	 JSONObject wifirecord  = new JSONObject();
+	    		        	 wifirecord.put("SSID", sr.SSID);
+	    		        	 wifirecord.put("Signal",sr.level);
+	    		        	 wifirecord.put("Frequency", sr.frequency);
+	    		        	 //wifirecord.put("BSSID",sr.BSSID);
+	    		        	 wifirecord.put("Capabilities", sr.capabilities);
+	    		        	 fix.put(sr.BSSID, wifirecord);
+	    		         }
+	    		         setChanged();
+	    		         notifyObservers(fix);
+    				}
+    				 Thread.sleep(interval);
     			 }
     			 catch(InterruptedException e){
     				 e.printStackTrace();
